1cb9ead9ab7de09de1933bb474ab4e2b40b58bc3
seunghee.Kim <tmdgmltmd7@naver.com>
2020-05-06 12:37:14 +0900

    branch1에추가

--- Test.java
+++ Test.java
@@ -1,7 +1,8 @@
 Class Test{
 	public static void main(String [] args){
 		Sysyem.out.println("Hello Git");
-		 System.out.println("Hi Git")
+		 System.out.println("Hi Git");
+		 System.out.println("branch1");
 
 	}
 }
