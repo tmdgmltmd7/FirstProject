664918fb1b9ff23cfb7b7d66e016ed1189a77d1f
seunghee.Kim <tmdgmltmd7@naver.com>
2020-05-06 12:58:44 +0900

    hello제거

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 Class Test{
 	public static void main(String [] args){
-		Sysyem.out.println("Hello Git");
+		Sysyem.out.println("qqqqq");
 		 System.out.println("Hi Git");
 		 System.out.println("branch1");
 		 System.out.println("branch2");
