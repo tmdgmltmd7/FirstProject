a84999f4640810e8b4e63608284132d4162cccff
seunghee.Kim <tmdgmltmd7@naver.com>
2020-05-06 11:02:47 +0900

    Test.java Hello ->heo

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 Class Test{
 	public static void main(String [] args){
-		Sysyem.out.println("Hello Git");
-		 System.out.println("Hi Gi")
+		Sysyem.out.println("Heo Git");
+		 System.out.println("Hi Git")
 
 	}
 }
---
+++ Test1.java
@@ -0,0 +1,5 @@
+Class Test1{
+	public static void main(String [] args){
+		System.out.println("Test1");
+	}
+}
